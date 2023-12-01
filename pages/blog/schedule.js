332b0d63cada9fd03db7b24a00955ef6cc099e97
yuumi <131945896+yuuuuuuuuuuumi@users.noreply.github.com>
2023-12-01 16:30:15 +0900

    1201_commit_03

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,4 +1,6 @@
 import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import Post from 'components/post-body'
@@ -6,8 +8,12 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/teo-colum
 import ConverBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Schedule({
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Post({
     title,
     publish,
     content,
@@ -16,6 +22,14 @@ export default function Schedule({
 }) {
     return (
         <container>
+            <Meta
+                pageTitle={title}
+                pageDesc={description}
+                pageImg={eyecatch.url}
+                pageImgW={eyecatch.width}
+                psgeImgH={eyecatch.height}
+            />
+
             <article>
                 <PostHeader title={title} subtitle="Blog Article" publish={publish} />
 
@@ -28,6 +42,8 @@ export default function Schedule({
                         height={eyecatch.height}
                         sizes="(min-width: 1152px) 1152px, 100vw"
                         priority
+                        placeholder="blur"
+                        blurDataURL={eyecatch.blurDataURL}
                     />
                 </figure>
 
@@ -48,17 +64,25 @@ export default function Schedule({
 
 
 export async function getStaticProps() {
-    const slug = 'schedule'
+    const slug = 'micro'
 
     const post = await getPostBySlug(slug)
+
+    const description = extractText(post.content)
+
+    const eyecatch = post.eyecatch ?? eyecatchLocal
+
+    const { base64 } = await getPlaiceholderr(eyecatch.url)
+    eyecatch.blurDataURL = base64
 
     return {
         props: {
             title: post.title,
             publish: post.publishDate,
             content: post.content,
-            eyecatch: post.eyecatch,
-            categories:post.categories,
+            eyecatch: eyecatch,
+            categories: post.categories,
+            description: description,
         },
     }
 }
