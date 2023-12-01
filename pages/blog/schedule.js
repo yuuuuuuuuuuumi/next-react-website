b66038b5694312a73b387cd029c062ea49c3c542
yuumi <131945896+yuuuuuuuuuuumi@users.noreply.github.com>
2023-12-01 16:06:38 +0900

    1201_commit_02

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,11 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import Post from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/teo-column'
+import ConverBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import Image from 'next/image'
 
 export default function Schedule({
     title,
@@ -13,6 +18,29 @@ export default function Schedule({
         <container>
             <article>
                 <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=""
+                        layout="responsive"
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        sizes="(min-width: 1152px) 1152px, 100vw"
+                        priority
+                    />
+                </figure>
+
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody>
+                            <ConvertBody contentHTML={content} />
+                        </PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar>
+                        <PostCategories categories={categories} />
+                    </TwoColumnSidebar>
+                </TwoColumn>
             </article>
         </container>
     )
